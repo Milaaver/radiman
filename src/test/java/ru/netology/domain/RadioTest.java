2a5ad0bc9748c3d671eb479513c728c04ead876e
Milaaver <84475809+Milaaver@users.noreply.github.com>
2021-11-16 18:46:27 +0300

    Update RadioTest.java

--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -2,90 +2,70 @@ package ru.netology.domain;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class RadioTest {
+    Radio radio = new Radio();
 
     @Test
-    void decreaseCurrentVolume() {
-        Radio radio = new Radio();
-
-        radio.setCurrentVolume(0);
-        radio.decreaseCurrentVolume();
-        assertEquals(0, radio.getCurrentVolume());
-        System.out.println("Volume:" + radio.getCurrentVolume());
+    void shouldSwitchStationUpToRound() {
+        radio.switchStationUp(); // 0~1
+        radio.switchStationUp(); // 1~2
+        radio.switchStationUp(); // 2~3
+        radio.switchStationUp(); // 3~4
+        radio.switchStationUp(); // 4~5
+        radio.switchStationUp(); // 5~6
+        radio.switchStationUp(); // 6~7
+        radio.switchStationUp(); // 7~8
+        radio.switchStationUp(); // 8~9
+        radio.switchStationUp(); // 9~0
+        assertEquals(0, radio.getStation());
     }
 
     @Test
-    void decreaseCurrentVolumeWell() {
-        Radio radio = new Radio();
-
-        radio.setCurrentVolume(5);
-        radio.decreaseCurrentVolume();
-        assertEquals(4, radio.getCurrentVolume());
-        System.out.println("Volume:" + radio.getCurrentVolume());
-    }
-
-
-    @Test
-    void decreaseCurrentStation() {
-        Radio radio = new Radio();
-
-        radio.setCurrentStation(0);
-        radio.decreaseCurrentStation();
-        assertEquals(9, radio.getCurrentStation());
-        System.out.println("Station:" + radio.getCurrentStation());
+    void shouldSwitchStationDownToRound() {
+        radio.switchStationDown(); // 0~9
+        radio.switchStationDown(); // 9~8
+        assertEquals(8, radio.getStation());
     }
 
     @Test
-    void decreaseCurrentStationWell() {
-        Radio radio = new Radio();
-
-        radio.setCurrentStation(5);
-        radio.decreaseCurrentStation();
-        assertEquals(4, radio.getCurrentStation());
-        System.out.println("Station:" + radio.getCurrentStation());
+    void shouldSpecifyStationPositive() {
+        radio.setStation(5);
+        assertEquals(5, radio.getStation());
     }
 
     @Test
-    void increaseCurrentStation() {
-        Radio radio = new Radio();
-
-        radio.setCurrentStation(9);
-        radio.increaseCurrentStation();
-        assertEquals(0, radio.getCurrentStation());
-        System.out.println("Station:" + radio.getCurrentStation());
+    void shouldSpecifyStationNegative() {
+        radio.setStation(50);
+        assertEquals(0, radio.getStation());
     }
 
     @Test
-    void increaseCurrentStationWell() {
-        Radio radio = new Radio();
-
-        radio.setCurrentStation(5);
-        radio.increaseCurrentStation();
-        assertEquals(6, radio.getCurrentStation());
-        System.out.println("Station:" + radio.getCurrentStation());
-    }
-
-
-    @Test
-    void increaseCurrentVolume() {
-        Radio radio = new Radio();
-
-        radio.setCurrentVolume(10);
-        radio.increaseCurrentVolume();
-        assertEquals(10, radio.getCurrentVolume());
-        System.out.println("Volume:" + radio.getCurrentVolume());
+    void shouldSpecifyStationNegative2() {
+        radio.setStation(-50);
+        assertEquals(0, radio.getStation());
     }
 
     @Test
-    void increaseCurrentVolumeWell() {
-        Radio radio = new Radio();
-
-        radio.setCurrentVolume(5);
-        radio.increaseCurrentVolume();
-        assertEquals(6, radio.getCurrentVolume());
-        System.out.println("Volume:" + radio.getCurrentVolume());
+    void shouldIncreaseVolume() {
+        radio.increaseVolume(); // 3~4
+        radio.increaseVolume(); // 4~5
+        radio.increaseVolume(); // 5~6
+        radio.increaseVolume(); // 6~7
+        radio.increaseVolume(); // 7~8
+        radio.increaseVolume(); // 8~9
+        radio.increaseVolume(); // 9~10
+        radio.increaseVolume(); // 10~10
+        assertEquals(10, radio.getVolume());
     }
 
+    @Test
+    void shouldDecreaseVolume() {
+        radio.decreaseVolume(); // 3~2
+        radio.decreaseVolume(); // 2~1
+        radio.decreaseVolume(); // 1~0
+        radio.decreaseVolume(); // 0~0
+        assertEquals(0, radio.getVolume());
+    }
 }
